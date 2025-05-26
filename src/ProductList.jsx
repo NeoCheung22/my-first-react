0caf1971b0fdc6d652f94483e14f862259bb3020
NeoCheung22 <neocheung22@hotmail.com>
2025-05-27 00:03:02 +0800

    update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import style from './ProductList.module.css'
+import styles from './ProductList.module.css'
 import {useState} from 'react'
 
 export default function ProductList() {
