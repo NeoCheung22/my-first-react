c381b39887d7fc354ccb9438fd1d1ef7f47e4454
NeoCheung22 <neocheung22@hotmail.com>
2025-06-12 02:31:24 +0800

    edited ProductList // useEffect

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import styles from './ProductList.module.css'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Title from './Title'
+import { use } from 'react'
 
 export default function ProductList() {
  
-  let [productList, setProductList] = useState([]) 
+  let [productList, setProductList] = useState([])
+  let [input, setInput] = useState('')
 
-  console.log(productList)
+  useEffect(() => {
+         //1 : 無第二個參數︰component 每次render時都會執行
+         //2 : Dependencies array是空array時 []︰component 只會在mount時執行一次
+        //3 : Dependencies array有值時 []︰第一次裝頁render時 + 指定的值有變時才會執行
 
-  fetch("https://neocheung22.github.io/demoapi/react-basicc-product.json")
-  .then(response => response.json())
-  .then(data => setProductList(data));
- 
+           fetch("https://neocheung22.github.io/demoapi/react-basicc-product.json")
+            .then(response => response.json())
+            .then(data => setProductList(data));
+          console.log(productList)
+  },[])  // Dependencies array
+
+  useEffect(() => {
+      if(input.length > 4) 
+        console.log("字串夠長")
+      else
+        console.log("字串太短")
+  },[input]) 
+
   return (
     <div>
-
+      <input type="text" onChange={e => setInput(e.target.value)} />
       <Title mainTitle="請選擇購買的水果" />
                
       <div> {/*水果清單*/}
